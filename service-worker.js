518e1d0a4bb7875a373b04a76cd7c3a2f407544b
1qazfs <109844840+1qazfs@users.noreply.github.com>
2026-08-04 09:59:26 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,20 @@
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
+const CACHE_NAME = 'pool-led-v1';
+const FILES_TO_CACHE = [
+  './',
+  './led_control.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
 });
-self.addEventListener('activate', function(e) {
-  return self.clients.claim();
-});
-self.addEventListener('fetch', function(e) {
-  e.respondWith(fetch(e.request));
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then(res => res || fetch(evt.request))
+  );
 });
